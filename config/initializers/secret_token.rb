f5eb857b9dbb820e00f22ed9bf7c0657f2674c37
Tim Chant <tjchant848@hotmail.com>
2013-04-23 23:14:08 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolReport::Application.config.secret_token = 'af4db17d0cf4910019e6835546fc9b7157f5cf92856ba064cb2ec4881e2b8f883c6f82bb06fc6a0bf4a5a6b44c290cbd4b536937e45a7dd39ea2293cf9163aa7'
