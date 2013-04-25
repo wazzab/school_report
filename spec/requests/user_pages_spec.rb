388bb3e55ac68d2036630c8b5e7949e4ec41da49
Tim Chant <tjchant848@hotmail.com>
2013-04-26 02:05:20 +1200

    Finish layout and routes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe "UserPages" do
   subject {page}
+  
   describe "Registration page" do
+    before {visit registration_path}
     it {should have_selector('h1', text: 'Registration')}
     it {should have_selector('title', text: full_title('Registration'))}
   end
